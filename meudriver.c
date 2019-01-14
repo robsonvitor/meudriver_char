4bc282262d294fb625d58bad5b37c008c2e70d68
Robson <robson@pc2035246.tco.ifsuldeminas.edu.br>
2019-01-14 17:32:58 -0200

    14 jan 2018 - 17:32

--- meudriver.c
+++ meudriver.c
@@ -10,37 +10,22 @@
 #include <asm/uaccess.h>
 
 static unsigned int major; /* variavel para o MAJOR do driver */
-static struct class *classe_meudriver;
-static struct cdev dummy_cdev;
-
-static char   message[256] = {0};           ///< Memory for the string that is passed from userspace
-static short  size_of_message;
+static struct class *meudriver_classe;
+static struct cdev meudriver_cdev;
 
 
 /*##################################################################*/
 
 static struct file* filp = NULL;
 
-struct file* file_open(const char* path, int flags, int rights)
-{
+struct file* file_open(const char* path, int flags, int rights){
     int err = 0;
     struct file* filp = NULL;
 
-
-    /* Para acessar uma região de memória que está além do limite de Endereço Virtual do Espaço do Usuário
-    (ou seja, caindo na região de Endereço Virtual do Espaço do Kernel), você primeiro armazena o limite atual. */
     mm_segment_t oldfs;
     oldfs = get_fs();
-
-    /* Define o limite como o do Kernel - 4 GB/*/
     set_fs(get_ds());
-
-
-    /* O get_fs() irá recuperar este limite e o set_fs() irá defini-lo com um valor */
-    /* Operações em memória (Ex: - Ler de um buffer que está no espaço do kernel de um contexto de usuário através de uma system call)*/
     filp = filp_open(path, flags, rights);
-
-    /* Define o limite de endereço de retorno para o limite original que foi armazenado na variável old_fs */
     set_fs(oldfs);
 
     if (IS_ERR(filp)) {
@@ -51,19 +36,55 @@ struct file* file_open(const char* path, int flags, int rights)
     return filp;
 }
 
-static int abre(struct inode *inode, struct file *file)
-{
+void file_close(struct file* file){
+    filp_close(file, NULL);
+}
+
+int file_read(struct file* file, unsigned long long offset, unsigned char* data, unsigned int size){
+    mm_segment_t oldfs;
+    int ret;
+
+    oldfs = get_fs();
+    set_fs(get_ds());
+    ret = vfs_read(file, data, size, &offset);
+    set_fs(oldfs);
+
+    return ret;
+}
+
+int file_write(struct file* file, unsigned long long offset, unsigned char* data, unsigned int size){
+    mm_segment_t oldfs;
+    int ret;
+
+    oldfs = get_fs();
+    set_fs(get_ds());
+    ret = vfs_write(file, data, size, &offset);
+    set_fs(oldfs);
+
+    return ret;
+}
+
+static int abre(struct inode *inode, struct file *file){
     filp = file_open("/dev/ttyACM0", O_RDWR, 0666);
     return 0;
 }
 
+static int libera(struct inode *inodep, struct file *filp){
+    file_close(filp);
+    return 0;
+}
+
+static ssize_t escrita(struct file *file, const char __user *buf,size_t len, loff_t *ppos){
+    ssize_t recv = file_write(filp, *ppos, buf, len);
+    return recv;
+}
+
 /*##################################################################*/
 struct file_operations meudriver_fops = {
-owner:       THIS_MODULE,
-open:       abre,
-// release:    fecha,
-// read:       leitura,
-// write:      escrita,
+    owner:       THIS_MODULE,
+    open:       abre,
+    release:    libera,
+    write:      escrita,
 };
 
 struct miscdevice meudriver_device = {
@@ -86,20 +107,19 @@ static int __init inicializa_driver(void) {
     pr_info("meudriver - meudriver_char major number = %d\n", major);
 
     /* Create device class, visible in /sys/class */
-    classe_meudriver = class_create(THIS_MODULE, "meudriver_char_class");
-    if (IS_ERR(classe_meudriver)) {
+    meudriver_classe = class_create(THIS_MODULE, "meudriver_char_class");
+    if (IS_ERR(meudriver_classe)) {
         pr_err("meudriver - Erro ao criar classe de caracter.\n");
         unregister_chrdev_region(MKDEV(major, 0), 1);
-        return PTR_ERR(classe_meudriver);
+        return PTR_ERR(meudriver_classe);
     }
 
     /* Initialize the char device and tie a file_operations to it */
-    cdev_init(&dummy_cdev, &meudriver_fops);
-    // dummy_cdev.owner = THIS_MODULE;
+    cdev_init(&meudriver_cdev, &meudriver_fops);
     /* Now make the device live for the users to access */
-    cdev_add(&dummy_cdev, devt, 1);
+    cdev_add(&meudriver_cdev, devt, 1);
 
-    meudriver_device = device_create(classe_meudriver,
+    meudriver_device = device_create(meudriver_classe,
                                      NULL,   /* no parent device */
                                      devt,    /* associated dev_t */
                                      NULL,   /* no additional data */
@@ -107,7 +127,7 @@ static int __init inicializa_driver(void) {
 
     if (IS_ERR(meudriver_device)) {
         pr_err("meudriver - Erro ao criar o dispositivo.\n");
-        class_destroy(classe_meudriver);
+        class_destroy(meudriver_classe);
         unregister_chrdev_region(devt, 1);
         return -1;
     }
@@ -118,9 +138,9 @@ static int __init inicializa_driver(void) {
 
 static void __exit remove_driver(void) {
     unregister_chrdev_region(MKDEV(major, 0), 1);
-    device_destroy(classe_meudriver, MKDEV(major, 0));
-    cdev_del(&dummy_cdev);
-    class_destroy(classe_meudriver);
+    device_destroy(meudriver_classe, MKDEV(major, 0));
+    cdev_del(&meudriver_cdev);
+    class_destroy(meudriver_classe);
 
     pr_info("meudriver - O driver foi removido!\n");
 }
--- tool.c
+++ tool.c
@@ -1,13 +1,3 @@
-/**
- * @file   testebbchar.c
- * @author Derek Molloy
- * @date   7 April 2015
- * @version 0.1
- * @brief  A Linux user space program that communicates with the ebbchar.c LKM. It passes a
- * string to the LKM and reads the response from the LKM. For this example to work the device
- * must be called /dev/ebbchar.
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
-*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<errno.h>
@@ -15,42 +5,43 @@
 #include<string.h>
 #include<unistd.h>
 
-#define BUFFER_LENGTH 256               ///< The buffer length (crude but fine)
-static char receive[BUFFER_LENGTH];     ///< The receive buffer from the LKM
+#define BUFFER_LENGTH 256
+static char receive[BUFFER_LENGTH];
 
 
 int main(){
    int ret, fd;
    char stringToSend[BUFFER_LENGTH];
-   printf("Ferramenta para escrever em ttyACM0\n");
-   fd = open("/dev/meudriver_char", O_RDWR);             // Open the device with read/write access
+
+   printf("Ferramenta para comunicacao com o Arduino na porta serial\n\n");
+
+   printf("Escreva o código no seguinte formato: R[0-255],G[0-255],B[0-255]\n\n");
+
+   printf("Exemplo de cores: \n");
+
+   printf("Vermelho: R255,G0,B0\n");
+   printf("Amarelo: R255,G255,B0\n");
+   printf("Verde: R0,G255,B0\n");
+   printf("#####################\n\n");
+
+
+   fd = open("/dev/meudriver_char", O_RDWR);
    if (fd < 0){
-      perror("Failed to open the device...");
+      perror("Erro ao abrir dispositivo!");
       return errno;
    }
 
    while(1){
       printf("Digite o codigo: ");
-      scanf("%[^\n]%*c", stringToSend);                // Read in a string (with spaces)
-      printf("Escrevendo mensagem na porta serial: [%s].\n", stringToSend);
-      ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
+      scanf("%s", stringToSend);
+      printf("Enviando codigo RGB para o Arduino: %s.\n", stringToSend);
+      ret = write(fd, stringToSend, strlen(stringToSend));
       if (ret < 0){
          perror("Falha ao escrever mensagem no driver.");
          return errno;
       }
 
-      // printf("Press ENTER to read back from the device...\n");
-      // getchar();
-
-      // printf("Reading from the device...\n");
-      // ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
-      // if (ret < 0){
-      //    perror("Failed to read the message from the device.");
-      //    return errno;
-      // }
-      // printf("The received message is: [%s]\n", receive);
    }
 
-   printf("End of the program\n");
    return 0;
 }
